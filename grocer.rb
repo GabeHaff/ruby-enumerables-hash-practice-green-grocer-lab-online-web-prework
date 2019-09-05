7359b1a558870504e52d522b6fb5ddccbc454f51
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 19:13:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,7 +24,7 @@ def apply_coupons(cart, coupons)
     elsif cart[item][:count]>=coupon[:num] && !cart["#{item} W/COUPON"]
     
 end 
-cart["#{item} W/COUPON"][:count] += coupon[:num]
+cart["#{item} W/COUPON"][:count]-= coupon[:num]
 end
 end 
 cart
